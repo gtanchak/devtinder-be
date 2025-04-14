d3f673f14403f26b1dcf22b711a5948475f5468e
gtanchak <ghtanchak77@gmail.com>
2025-04-15 00:09:15 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.use("/user", (req, res) => {
+  res.send("User from the server.");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running successfully on PORT ${PORT}`);
+});
