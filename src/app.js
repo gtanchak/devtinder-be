3bba09a5196cbb0d0d5399e374091766f2841f76
gtanchak <ghtanchak77@gmail.com>
2025-04-27 11:51:10 +0530

    fix: update the signup API with dynamic data

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const { connectDB } = require("./config/database");
+const { User } = require("./models/user");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+// Convert JSON to js object
+app.use(express.json());
+
+app.post("/signup", (req, res) => {
+  const user = new User(req.body);
+  try {
+    user.save();
+    res.send("User created successfully.");
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
 
 connectDB()
   .then(() => {
---
+++ src/models/user.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  age: Number,
+  gender: String,
+  password: String,
+});
+
+const User = model("User", userSchema);
+
+module.exports = { User };
